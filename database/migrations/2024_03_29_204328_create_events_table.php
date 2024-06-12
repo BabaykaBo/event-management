b2cf7a0a40eebc073fb0286f9b9481c7ab245e34
Oleh <zaaqq135@gmail.com>
2024-06-12 21:06:21 +0300

    added factory and seeder

--- database/migrations/2024_03_29_204328_create_events_table.php
+++ database/migrations/2024_03_29_204328_create_events_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
             $table->foreignIdFor(User::class);
             $table->text('description')->nullable();
             $table->dateTime('start_time');
