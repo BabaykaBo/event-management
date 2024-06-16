319ddfe29c34f9eaa21f32350b85a42da95ca803
Oleh <zaaqq135@gmail.com>
2024-06-16 16:15:20 +0300

    created command for reminding users about events

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command for reminding about events';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')
+        ->whereBetween('start_time', [now(), now()->addDay()])
+        ->get();
+
+        $eventsCount = $events->count();
+        $eventLabel = Str::plural('event', $eventsCount);
+
+        $this->info("Found $eventsCount $eventLabel");
+
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $this->info(
+                    "Notifying the user {$attendee->user->id}"
+                )
+            )
+        );
+
+        $this->info('Reminder notifications sent successfully');
+    }
+}
